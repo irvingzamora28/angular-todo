8723b5cbe9f00a5ae127b0ddf4a4b4051ef6e226
Irving Zamora <irvingzamora28@gmail.com>
2021-08-23 00:40:24 -0600

    Toggle reminder

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -9,6 +9,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent implements OnInit {
   @Input() task: Task = {text: '', day: '', reminder: false};
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder:EventEmitter<Task> = new EventEmitter()
   faTimes = faTimes
 
   constructor() { }
@@ -18,5 +19,9 @@ export class TaskItemComponent implements OnInit {
 
   onDelete(task: Task) {
     this.onDeleteTask.emit(task)
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task)
   }
 }
